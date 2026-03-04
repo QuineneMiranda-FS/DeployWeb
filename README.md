5be4ea00ec73a410f0ead94e6e6956de1d659f4b
Miranda Quinene <mequinene@student.fullsail.edu>
2026-03-04 09:45:21 -0500

    port setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WDV353
+
 Server Side Languages
+
+## Week 1
+
+-Setup Environment: NPM, NODE Modules, NodeJS, GitHub, Postman
+-Create NodeJS code
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const http = require("http");
+require("dotenv").config();
 
-const server = http.createServer((req, res) => {
-  //   const { headers, url, method } = req;
-  //   console.log(Headers, url, method);
-  res.setHeader("Content-Type", "text/plain");
-  res.write("<h1>Hello World</h1>");
-  res.end();
+http.createServer.listen(process.env.PORT, () => {
+  console.log("server is running on port", process.env.PORT);
 });
 
-const PORT = 3000;
+// const server = http.createServer((req, res) => {
+//   const { headers, url, method } = req;
+//   console.log(Headers, url, method);
+//   res.setHeader("Content-Type", "text/plain");
+//   res.write("<h1>Hello World</h1>");
+//   res.end();
+// });
 
-server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
+// const PORT = 3000;
+
+// server.listen(PORT, () => {
+//   console.log(`listening on ${PORT}`);
+// });
