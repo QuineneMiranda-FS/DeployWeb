cffb862be479585c2091deab7cfda1bde13c2850
Miranda Quinene <mequinene@student.fullsail.edu>
2026-03-08 21:29:52 -0400

    setup jest test assgn

---
+++ math/math.js
@@ -0,0 +1,15 @@
+const add;
+const subtract;
+const divide;
+const multiply;
+const squareRoot;
+const maxNum;
+
+module.exports = {
+  add,
+  subtract,
+  divide,
+  multiply,
+  squareRoot,
+  maxNum,
+};
---
+++ math/math.test.js
@@ -0,0 +1,22 @@
+const {
+  add,
+  subtract,
+  divide,
+  multiply,
+  squareRoot,
+  maxNum,
+} = require("./math");
+
+describe("testing math", () => {
+  test("take in 2 numbers and return the sum", () => {
+    //
+    expect(add("")).toBe("");
+  });
+});
+
+// Your subtract function will take in 2 parameters and return the difference
+// Your multiply function will take in 2 numbers and return the product
+// Your divide function will take in 2 parameters and return the quotient
+// Also, write 2 additional tests using JavaScript's Math Object. The two tests will be square root and max.
+// Your sqrt function will take in a number and return the square root - (Use Math's sqrt function)
+// Your max function will take in 2 parameters and return the max. - (Use Math's max function)
