65872fea6cdd922b5b20a22c2081cad1588a36af
Miranda Quinene <mequinene@student.fullsail.edu>
2026-03-08 20:30:40 -0400

    added jest and setup test folder

---
+++ testViaJest/viaJest.test.js
@@ -0,0 +1,9 @@
+// You are going to write your own unit test using the Math module using Jest.
+// You are to write tests and code for your math module that will add, subtract, multiply, and divide.
+// Your add function will take in 2 numbers and return the sum
+// Your subtract function will take in 2 parameters and return the difference
+// Your multiply function will take in 2 numbers and return the product
+// Your divide function will take in 2 parameters and return the quotient
+// Also, write 2 additional tests using JavaScript's Math Object. The two tests will be square root and max.
+// Your sqrt function will take in a number and return the square root - (Use Math's sqrt function)
+// Your max function will take in 2 parameters and return the max. - (Use Math's max function)
