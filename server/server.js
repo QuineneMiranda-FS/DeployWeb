c89ce14e83af911833f083e9819c91ef35bd9558
Miranda Quinene <mequinene@student.fullsail.edu>
2026-04-20 20:52:03 -0400

    fix port

--- server/server.js
+++ server/server.js
@@ -10,6 +10,11 @@ const app = require("./api");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server is on ${PORT}`);
+// });
+
+// '0.0.0.0' for  Render
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is on ${PORT}`);
 });
