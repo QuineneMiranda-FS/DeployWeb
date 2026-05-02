0df64770b1ea87f366f100276c743b6c3cfa77cc
Miranda Quinene <mequinene@student.fullsail.edu>
2026-05-01 20:01:54 -0400

    added mdlwre for hooks

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 
 const app = express();
 const routeHandler = require("./routes");
+const app = express();
+
+app.set("etag", false);
 
 // const corsOptions = {
 //   origin: "*", // Allows any device (like your phone/emulator)
@@ -19,6 +22,17 @@ app.use(cors());
 //body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//middleware
+app.use((req, res, next) => {
+  res.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate",
+  );
+  res.set("Pragma", "no-cache");
+  res.set("Expires", "0");
+  next();
+});
+
 //flag http calls
 app.use(morgan("dev"));
 
